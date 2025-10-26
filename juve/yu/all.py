bc0f35442f6f131382490536b119da36ed42dfae
Ethan Alker <ealker1@gmail.com>
2025-10-25 22:34:26 -0400

    Add JSON output to yu/all.py

--- juve/yu/all.py
+++ juve/yu/all.py
@@ -8,6 +8,11 @@ if __name__ == '__main__':
     fmri.main("-D", "fmri.xml")
     protein.main("-D", "protein.xml")
     
+    # JSON files
+    pipeline.main("-j", "pipeline.json")
+    fmri.main("-j", "fmri.json")
+    protein.main("-j", "protein.json")
+    
     # DOT files
     pipeline.main("-d", "pipeline.dot")
     fmri.main("-d", "fmri.dot")
